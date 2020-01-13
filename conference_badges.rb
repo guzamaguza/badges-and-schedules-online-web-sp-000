90fa8bda637a7c44acc27ff5d9842831a288ccbb
Mark A Guza <guzamaguza@gmail.com>
2020-01-13 01:27:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-
+  name_array = []
+  array.each do |name|
+    name_array.push("Hello, my name is #{name}.")
+  end
+  return name_array
 end
 
 def assign_rooms()
