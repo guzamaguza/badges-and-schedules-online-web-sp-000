d5214277f8f586e3cb3ef14e71a3468865c7f4f0
Mark A Guza <guzamaguza@gmail.com>
2020-01-13 01:57:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,7 @@ def assign_rooms(speakers)
 end
 
 
-def printer
-  puts batch_badge_creator
-  puts assign_rooms
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end
